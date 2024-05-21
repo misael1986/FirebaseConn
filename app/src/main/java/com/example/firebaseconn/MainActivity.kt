7f253ab56fb5cefadd02b67354addb728ad89a40
misael.perilla <misael.perilla@lemonade.com>
2024-05-20 20:10:33 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firebaseconn/MainActivity.kt
@@ -0,0 +1,98 @@
+package com.example.firebaseconn
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.OpenableColumns
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+
+class MainActivity : AppCompatActivity() {
+
+    // creating a storage reference
+    val storageRef = Firebase.storage.reference;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+       val btnSelectImage = findViewById<Button>(R.id.button)
+        // button Click listener
+        // invoke on user interaction
+        btnSelectImage.setOnClickListener {
+            // PICK INTENT picks item from data
+            // and returned selected item
+            val galleryIntent = Intent(Intent.ACTION_PICK)
+            // here item is type of image
+            galleryIntent.type = "image/*"
+            // ActivityResultLauncher callback
+            imagePickerActivityResult.launch(galleryIntent)
+        }
+
+    }
+
+    private var imagePickerActivityResult: ActivityResultLauncher<Intent> =
+    // lambda expression to receive a result back, here we
+        // receive single item(photo) on selection
+        registerForActivityResult( ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result != null) {
+                // getting URI of selected Image
+                val imageUri: Uri? = result.data?.data
+
+                //val fileName = imageUri?.pathSegments?.last()
+                val imageview = findViewById<ImageView>(R.id.imageView)
+
+                // extract the file name with extension
+                val sd = getFileName(applicationContext, imageUri!!)
+
+                // Upload Task with upload to directory 'file'
+                // and name of the file remains same
+                val uploadTask = storageRef.child("file/$sd").putFile(imageUri)
+
+                // On success, download the file URL and display it
+                uploadTask.addOnSuccessListener {
+                    // using glide library to display the image
+                    storageRef.child("upload/$sd").downloadUrl.addOnSuccessListener {
+                        Glide.with(this@MainActivity)
+                            .load(it)
+                            .into(imageview).view
+
+                        Log.e("Firebase", "download passed")
+                        Toast.makeText(this,"Imagen "+sd.toString()+"cargada a Firebase con exito",Toast.LENGTH_LONG)
+                    }.addOnFailureListener {
+                        Log.e("Firebase", "Failed in downloading")
+                    }
+                }.addOnFailureListener {
+                    Log.e("Firebase", "Image Upload fail")
+                    Toast.makeText(this,"Imagen "+sd.toString()+" Falló en la carga a  Firebase",Toast.LENGTH_LONG)
+                }
+            }
+        }
+
+    @SuppressLint("Range")
+    private fun getFileName(context: Context, uri: Uri): String? {
+        if (uri.scheme == "content") {
+            val cursor = context.contentResolver.query(uri, null, null, null, null)
+            cursor.use {
+                if (cursor != null) {
+                    if(cursor.moveToFirst()) {
+                        return cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME))
+                    }
+                }
+            }
+        }
+        return uri.path?.lastIndexOf('/')?.let { uri.path?.substring(it) }
+    }
+
+
+}
